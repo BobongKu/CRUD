6cf11ed69379e08469ff28fea0938e4b3f475eb7
Bobong <jhbgjy7@gmail.com>
2023-10-12 14:09:14 +0900

    Custom FileException 생성

---
+++ src/main/java/bobong/crud/global/file/exception/FileException.java
@@ -0,0 +1,18 @@
+package bobong.crud.global.file.exception;
+
+import bobong.crud.global.exception.BaseException;
+import bobong.crud.global.exception.BaseExceptionType;
+
+public class FileException extends BaseException {
+    private BaseExceptionType exceptionType;
+
+
+    public FileException(BaseExceptionType exceptionType) {
+        this.exceptionType = exceptionType;
+    }
+
+    @Override
+    public BaseExceptionType getExceptionType() {
+        return exceptionType;
+    }
+}
