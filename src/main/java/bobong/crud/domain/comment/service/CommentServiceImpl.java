3deb114d314cf89e5bb800320d74e2d621ec4c08
Bobong <jhbgjy7@gmail.com>
2023-10-12 16:20:38 +0900

    save 기능 변경

--- src/main/java/bobong/crud/domain/comment/service/CommentServiceImpl.java
+++ src/main/java/bobong/crud/domain/comment/service/CommentServiceImpl.java
@@ -1,8 +1,18 @@
 package bobong.crud.domain.comment.service;
 
+import bobong.crud.domain.comment.dto.CommentSaveDto;
+import bobong.crud.domain.comment.dto.CommentUpdateDto;
 import bobong.crud.domain.comment.entity.Comment;
+import bobong.crud.domain.comment.exception.CommentException;
+import bobong.crud.domain.comment.exception.CommentExceptionType;
 import bobong.crud.domain.comment.repository.CommentRepository;
+import bobong.crud.domain.member.repository.MemberRepository;
+import bobong.crud.domain.post.exception.PostException;
+import bobong.crud.domain.post.exception.PostExceptionType;
+import bobong.crud.domain.post.repository.PostRepository;
+import bobong.crud.global.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,27 +24,47 @@ import java.util.List;
 public class CommentServiceImpl implements CommentService{
 
     private final CommentRepository commentRepository;
+    private final MemberRepository memberRepository;
+    private final PostRepository postRepository;
 
     @Override
-    public void save(Comment comment) {
+    public void save(Long postId, CommentSaveDto commentSaveDto) {
+        Comment comment = commentSaveDto.toEntity();
+
+        comment.confirmWriter(memberRepository.findByUsername(SecurityUtil.getLoginUsername()).orElseThrow(() -> new UsernameNotFoundException("이잉")));
+
+        comment.confirmPost(postRepository.findById(postId).orElseThrow(() -> new PostException(PostExceptionType.POST_NOT_POUND)));
+
         commentRepository.save(comment);
     }
 
     @Override
-    @Transactional(readOnly = true)
-    public Comment findById(Long id) throws Exception {
-        return commentRepository.findById(id).orElseThrow(() -> new Exception("댓글이 존재하지 않습니다."));
+    public void saveReComment(Long postId, Long parentId, CommentSaveDto commentSaveDto) {
+        Comment comment = commentSaveDto.toEntity();
+
+        comment.confirmWriter(memberRepository.findByUsername(SecurityUtil.getLoginUsername()).orElseThrow(() -> new UsernameNotFoundException("이잉")));
+
+        comment.confirmPost(postRepository.findById(postId).orElseThrow(() -> new PostException(PostExceptionType.POST_NOT_POUND)));
+
+        comment.confirmParent(commentRepository.findById(parentId).orElseThrow(() -> new CommentException(CommentExceptionType.NOT_POUND_COMMENT)));
+
+        commentRepository.save(comment);
     }
 
     @Override
-    @Transactional(readOnly = true)
-    public List<Comment> findAll() {
-        return commentRepository.findAll();
+    public void update(Long id, CommentUpdateDto commentUpdateDto) {
+        Comment comment = commentRepository.findById(id).orElseThrow(() -> new CommentException(CommentExceptionType.NOT_POUND_COMMENT));
+
+        if(!comment.getWriter().getUsername().equals(SecurityUtil.getLoginUsername())) {
+            throw new CommentException(CommentExceptionType.NOT_AUTHORITY_UPDATE_COMMENT);
+        }
+
+        commentUpdateDto.content().ifPresent(comment::updateContent);
     }
 
     @Override
-    public void remove(Long id) throws Exception {
-        Comment comment = commentRepository.findById(id).orElseThrow(() -> new Exception("댓글이 존재하지 않습니다."));
+    public void remove(Long id){
+        Comment comment = commentRepository.findById(id).orElseThrow(() -> new CommentException(CommentExceptionType.NOT_POUND_COMMENT));
         comment.remove(); //쿼리 수정
         List<Comment> removableCommentList = comment.findRemovableList();
         removableCommentList.forEach(removableComment -> commentRepository.delete(removableComment)); //실제 삭제
