54e98d9805d9971f782235749a3f85752663d78e
Bobong <jhbgjy7@gmail.com>
2023-10-18 21:18:07 +0900

    querydsl을 통한 post 검색 기능 구현

---
+++ src/main/java/bobong/crud/domain/post/cond/PostSearchCondition.java
@@ -0,0 +1,8 @@
+package bobong.crud.domain.post.cond;
+
+import lombok.Data;
+
+@Data
+public class PostSearchCondition {
+    private String keyword;
+}
--- src/main/java/bobong/crud/domain/post/repository/PostQueryRepository.java
+++ src/main/java/bobong/crud/domain/post/repository/PostQueryRepository.java
@@ -1,15 +1,17 @@
 package bobong.crud.domain.post.repository;
 
-import bobong.crud.domain.member.entity.QMember;
+import bobong.crud.domain.post.cond.PostSearchCondition;
 import bobong.crud.domain.post.entity.Post;
-import bobong.crud.domain.post.entity.QPost;
+import bobong.crud.domain.post.service.PostService;
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -27,18 +29,33 @@ public class PostQueryRepository {
         this.query = new JPAQueryFactory(em);
     }
 
-    public Page<Post> getPostList(Pageable pageable) {
+    public Page<Post> getPostList(Pageable pageable, PostSearchCondition condition) {
         QueryResults<Post> results = query
                 .selectFrom(post)
+                .where(titleHasStr(condition.getKeyword())
+                        .or(contentHasStr(condition.getKeyword()))
+                        .or(writerHasStr(condition.getKeyword())))
                 .leftJoin(post.writer, member).fetchJoin()
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
-                .orderBy(post.id.desc())
+                .orderBy(post.createdDate.desc())
                 .fetchResults();
 
         List<Post> content = results.getResults();
         Long total = results.getTotal();
 
         return new PageImpl<>(content, pageable, total);
+    }
+
+    private BooleanExpression contentHasStr(String keyword) {
+        return StringUtils.hasLength(keyword) ? post.content.contains(keyword) : post.content.contains("");
+    }
+
+    private BooleanExpression titleHasStr(String keyword) {
+        return StringUtils.hasLength(keyword) ? post.title.contains(keyword) : post.title.contains("");
+    }
+
+    private BooleanExpression writerHasStr(String keyword) {
+        return StringUtils.hasLength(keyword) ? post.writer.nickname.contains(keyword) : post.writer.nickname.contains("");
     }
 }
